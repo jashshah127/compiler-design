8064c2d0223bc0229969a77f34938147e8ccaea1
Jash Bhavesh Shah <46564241+jashshah127@users.noreply.github.com>
2020-01-07 17:08:13 +0530

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,47 +1,29 @@
-#include<stdio.h>
+#include <iostream>
 #include<conio.h>
-#include<string.h>
-int main()
+
+class product
 {
-	
-	char string[25];
-	int count=0,flag;
-	printf("enter any string: ");
-	gets(string);
-	if( (string[0]>='a'&&string[0]<='z') //small letter
-	    ||
-	    (string[0]>='A'&&string[0]<='Z') //cap letter
-	    ||
-	    (string[0]=='_')   //underscore
-	   )
-	{
-	   for(int i=1;i<=strlen(string);i++)
-	   {
-	     if((string[i]>='a'&& string[i]<='z')
-	     ||
-	      (string[i]>='A' && string[i]<='Z')
-	     ||
-	     (string[i]>='0'&& string[i]<='9')
-	     ||
-	     (string[i]=='-')
-	     )
-	     {
-			 count++;
-	     }
-	   }
-	    if(count==strlen(string))
-	    {
-	      flag=0;
-	    }
-	}
-	else
-	{
-		flag=1;
-	}
-	if(flag==1)
-	 	printf("%s is not valid identifier",string);
-	else
-		printf("%s is valid identifier",string);
-	getch();
+	int number ;
+	float cost;
+	public :
+		void getdata(int a , float b);
+		void putdata()
+		{
+			cout<<"number = "<<number ;
+			cout<<"cost ="<<cost;
+		}
+};
+void product :: getdata(int a , float b)
+{
+	number = a;
+	cost = b;
 }
 
+void main()
+{
+	product p;
+	p.getdata(7,7.2);
+	p.putdata();
+	
+		getch();
+}
